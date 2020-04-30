dad7e28e77500f451fd2da66591a3ef3f7d260e4
Crystal Lopez <crystallopez289@gmail.com>
2020-04-30 23:24:25 +0000

    completed the custom_len function

--- list-slicing/further_study.py
+++ list-slicing/further_study.py
@@ -15,8 +15,11 @@ def custom_len(input_list):
         8
 
     """
+    count = 0
+    for i in input_list:
+        count = count+1
 
-    return 0
+    return count
 
 
 # For the next four exercises, you'll need to be clever and think about ways
