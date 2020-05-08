6b019ec3739963146003062b0632362ecec8bf59
Crystal Lopez <crystallopez289@gmail.com>
2020-05-08 22:51:23 +0000

    completed part 2/xmas melons base price *1.5, flat fee to intl orders < 10 melons

--- oo-melons/melons.py
+++ oo-melons/melons.py
@@ -16,6 +16,9 @@ class AbstractMelonOrder():
 
         base_price = 5
 
+        if self.species == "christmas melon": 
+            base_price = base_price*1.5
+
         total = (1 + self.tax) * self.qty * base_price
         return total
 
@@ -44,3 +47,13 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+
+        total = super().get_total()
+
+        if self.qty < 10:
+
+            total = total+3
+
+        return total
