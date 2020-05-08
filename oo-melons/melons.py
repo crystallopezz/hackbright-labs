bd3b1afa8beb5dc6b0a0eccb6ba2904fbf5f8278
Crystal Lopez <crystallopez289@gmail.com>
2020-05-08 22:49:40 +0000

    completed part 1/created abstract melon class

--- oo-melons/melons.py
+++ oo-melons/melons.py
@@ -1,8 +1,6 @@
 """Classes for melon orders."""
 
-
-class DomesticMelonOrder():
-    """A melon order within the USA."""
+class AbstractMelonOrder():
 
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
@@ -10,15 +8,15 @@ class DomesticMelonOrder():
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
+        self.order_type = ""
+        self.tax = 0
 
     def get_total(self):
         """Calculate price, including tax."""
 
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
 
+        total = (1 + self.tax) * self.qty * base_price
         return total
 
     def mark_shipped(self):
@@ -26,32 +24,21 @@ class DomesticMelonOrder():
 
         self.shipped = True
 
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    order_type = "domestic"
+    tax = .08
 
-class InternationalMelonOrder():
+
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
+    order_type = "international"
+    tax = .17
 
     def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes."""
-
-        self.species = species
-        self.qty = qty
+        super().__init__(species, qty)
         self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
 
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
